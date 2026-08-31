884a4f2726b36012a8c2f3a8c0791bbc777b5e26
Aaron Kable <aaronkable@gmail.com>
2026-08-31 17:57:43 +0800

    fbump

--- wh_mapper/__init__.py
+++ wh_mapper/__init__.py
@@ -1,6 +1,6 @@
 """Wormhole Mapper for Alliance Auth"""
 
-__version__ = "1.0.0a1"
+__version__ = "1.0.0a2"
 __title__ = "YAWN"
 __compat_date__ = "2026-08-18"
 __url__ = "https://github.com/aaronkable/aa-wh-mapper"
